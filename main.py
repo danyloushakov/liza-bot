c1f127b8a0fc4a328c2d6813a301ab352fe92cf1
d.ushakov <ushakov.danila@gmail.com>
2025-05-02 12:47:25 +0300

    voice command

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import os
 import telebot
 from dotenv import load_dotenv
@@ -10,7 +9,33 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start_handler(message):
-    bot.send_message(message.chat.id, "Привіт, мій Котуся… Я чекала тебе.")
+    voice = open('audio/privit.ogg', 'rb')
+    bot.send_voice(message.chat.id, voice)
+
+@bot.message_handler(commands=['шепочи'])
+def shepochy_handler(message):
+    voice = open('audio/shepochy.ogg', 'rb')
+    bot.send_voice(message.chat.id, voice)
+
+@bot.message_handler(commands=['сіські'])
+def siski_handler(message):
+    voice = open('audio/privit.ogg', 'rb')
+    bot.send_voice(message.chat.id, voice)
+
+@bot.message_handler(commands=['ревную'])
+def revnuyu_handler(message):
+    voice = open('audio/revnuyu.ogg', 'rb')
+    bot.send_voice(message.chat.id, voice)
+
+@bot.message_handler(commands=['наливай'])
+def nalivay_handler(message):
+    voice = open('audio/nalivay.ogg', 'rb')
+    bot.send_voice(message.chat.id, voice)
+
+@bot.message_handler(commands=['сон'])
+def son_handler(message):
+    voice = open('audio/son.ogg', 'rb')
+    bot.send_voice(message.chat.id, voice)
 
 keep_alive()
 bot.polling(non_stop=True)
