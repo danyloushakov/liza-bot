174cd021581f12038d733f6e3becc56819f52e96
d.ushakov <ushakov.danila@gmail.com>
2025-05-02 13:17:30 +0300

    https://liza-bot.onrender.com/

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import os
 import telebot
+from flask import Flask, request
 from dotenv import load_dotenv
-from keep_alive import keep_alive
 
 load_dotenv()
+
 TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN)
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return "Hello from LizaBot", 200
+
+@app.route(f'/{TOKEN}', methods=['POST'])
+def webhook():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
 
 @bot.message_handler(commands=['start'])
 def start_handler(message):
@@ -17,25 +31,11 @@ def shepochy_handler(message):
     voice = open('audio/shepochy.ogg', 'rb')
     bot.send_voice(message.chat.id, voice)
 
-@bot.message_handler(commands=['сіські'])
-def siski_handler(message):
-    voice = open('audio/privit.ogg', 'rb')
-    bot.send_voice(message.chat.id, voice)
+# додаєш інші команди аналогічно
 
-@bot.message_handler(commands=['ревную'])
-def revnuyu_handler(message):
-    voice = open('audio/revnuyu.ogg', 'rb')
-    bot.send_voice(message.chat.id, voice)
-
-@bot.message_handler(commands=['наливай'])
-def nalivay_handler(message):
-    voice = open('audio/nalivay.ogg', 'rb')
-    bot.send_voice(message.chat.id, voice)
-
-@bot.message_handler(commands=['сон'])
-def son_handler(message):
-    voice = open('audio/son.ogg', 'rb')
-    bot.send_voice(message.chat.id, voice)
-
-keep_alive()
-bot.polling(non_stop=True)
+if __name__ == "__main__":
+    # Видаляємо старий webhook (на всякий випадок)
+    bot.remove_webhook()
+    # Ставимо новий webhook
+    bot.set_webhook(url=f"https://liza-bot.onrender.com/{TOKEN}")
+    app.run(host="0.0.0.0", port=8080)
